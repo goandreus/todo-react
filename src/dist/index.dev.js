9eac1de65ccff69558f35bef398774927a74925f
goandreus <andres.chavez.crisostomo@gmail.com>
2020-08-24 00:40:25 -0500

    variable y constantes and scope

--- src/dist/index.dev.js
+++ src/dist/index.dev.js
@@ -1,3 +1,17 @@
 "use strict";
 
-console.log('Hola mundo');
+// variables y constantes
+var nombre = 'Andres';
+var apellido = 'Chavez';
+var valorDado = 5;
+valorDado = 4;
+console.log(nombre, apellido, valorDado); //scope
+
+if (true) {
+  var _nombre = 'Carlos';
+  console.log(_nombre);
+  var _valorDado = 6;
+  console.log(_valorDado);
+}
+
+console.log(valorDado);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Hola mundo');
+// variables y constantes
+
+const nombre = 'Andres';
+const apellido = 'Chavez';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre,apellido,valorDado);
+
+//scope
+if(true){
+    const nombre = 'Carlos';
+    console.log(nombre)
+    let valorDado = 6
+    console.log(valorDado)
+}
+
+console.log(valorDado);
