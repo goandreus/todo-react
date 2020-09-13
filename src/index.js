ff139ee56fec5f3a962334cbde87d5422d7e00c4
goandreus <andres.chavez.crisostomo@gmail.com>
2020-09-13 17:25:06 -0500

    prmise

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
-import { heroes } from './data/heroes';
+const promesa = new Promise( (resolve, reject) => {
+    setTimeout( () => {
+        console.log('2 seconds later')
+    }, 2000)
+});
 
-// const getHeroeById = (id) => {
-//     return heroes.find((heroe) =>{
-//         if(heroe.id === id){
-//             return true;
-//         }else{
-//             return false;
-//         }
-//     });
-// }
-
-const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id);
-
-console.log(getHeroeById(2));
-
-const getHeroeByOwner = (owner) => heroes.filter( (heroe) => heroe.owner === owner);
-
-console.log(getHeroeByOwner('DC'));
